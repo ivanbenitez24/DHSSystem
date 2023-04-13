d41be3c5ad2b0719a25573ae2a6ec2b1277fae63
IVAN AMER BENITEZ <60254247+ivanbenitez24@users.noreply.github.com>
2023-04-13 01:32:31 +0000

    modified:   system/a_st_list_edit.php

--- system/a_st_list_edit.php
+++ system/a_st_list_edit.php
@@ -12,15 +12,18 @@
 </head>
 <body>
 	<h2>Edit Student Profile</h2>
-	<form method="POST" action="update.php?id=<?php echo $id; ?>">
-		<label>Student ID</label><input type="text" value="<?php echo $row['st_id']; ?>" name="st_id">
-		<label>Student Name</label><input type="text" value="<?php echo $row['st_name']; ?>" name="st_name">
-        <label>Year Level</label><input type="text" value="<?php echo $row['st_yr']; ?>" name="st_yr">
-        <label>Course</label><input type="text" value="<?php echo $row['st_course']; ?>" name="st_course">
-        <label>HK Type</label><input type="text" value="<?php echo $row['st_hktype']; ?>" name="st_hktype">
-        <label>Email</label><input type="text" value="<?php echo $row['st_email']; ?>" name="st_email">
-		<input type="submit" name="submit">
-		<a href="a_st_list.php">Back</a>
-	</form>
+    
+    <div class="section" id="editForm">
+        <form method="POST" action="update.php?id=<?php echo $id; ?>">
+            <label>Student ID</label><input type="text" value="<?php echo $row['st_id']; ?>" name="st_id">
+            <label>Student Name</label><input type="text" value="<?php echo $row['st_name']; ?>" name="st_name">
+            <label>Year Level</label><input type="text" value="<?php echo $row['st_yr']; ?>" name="st_yr">
+            <label>Course</label><input type="text" value="<?php echo $row['st_course']; ?>" name="st_course">
+            <label>HK Type</label><input type="text" value="<?php echo $row['st_hktype']; ?>" name="st_hktype">
+            <label>Email</label><input type="text" value="<?php echo $row['st_email']; ?>" name="st_email">
+            <input type="submit" name="submit">
+            <a href="a_st_list.php">Back</a>
+        </form>
+    </div>
 </body>
 </html>
