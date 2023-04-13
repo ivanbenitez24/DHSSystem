d203e0095a55eca0c5369ab1e68a68c8da439163
IVAN AMER BENITEZ <60254247+ivanbenitez24@users.noreply.github.com>
2023-04-13 02:30:12 +0000

    modified:   system/a_st_list_del.php

--- system/a_st_list_del.php
+++ system/a_st_list_del.php
@@ -1,6 +1,6 @@
 <?php
     include('a_st_list.php');
-	$id=$_GET['st_id'];
+	$id=$_GET['id'];
 	include('db_conn.php');
 	mysqli_query($conn,"DELETE FROM `student_pfp` where st_id='$id'");
 	header('location:a_st_list.php');
